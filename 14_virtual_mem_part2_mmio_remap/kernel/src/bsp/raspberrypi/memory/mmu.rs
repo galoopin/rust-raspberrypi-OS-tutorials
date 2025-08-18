b205e1013422c4b164bf53c58e075715d88a8c9e
Galoopin <aurelien.cibert@gmail.com>
2025-08-18 17:36:44 +0200

    14_virtual_mem_part2_mmio_remap: fix mmu `size_to_num_pages` helper function
    clippy lints error that force usage of `is_multiple_of` instead of manual divisibility check

--- 14_virtual_mem_part2_mmio_remap/kernel/src/bsp/raspberrypi/memory/mmu.rs
+++ 14_virtual_mem_part2_mmio_remap/kernel/src/bsp/raspberrypi/memory/mmu.rs
@@ -53,7 +53,7 @@ static KERNEL_TABLES: InitStateLock<KernelTranslationTable> =
 /// Helper function for calculating the number of pages the given parameter spans.
 const fn size_to_num_pages(size: usize) -> usize {
     assert!(size > 0);
-    assert!(size % KernelGranule::SIZE == 0);
+    assert!(size.is_multiple_of(KernelGranule::SIZE));
 
     size >> KernelGranule::SHIFT
 }
