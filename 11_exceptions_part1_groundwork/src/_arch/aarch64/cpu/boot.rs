094f80ce3677ee719c2baa1253cbfde1f127f80a
Galoopin <aurelien.cibert@gmail.com>
2025-08-18 21:10:23 +0200

    11_exceptions_part1_groundwork: migrate to Rust 2024 edition

--- 11_exceptions_part1_groundwork/src/_arch/aarch64/cpu/boot.rs
+++ 11_exceptions_part1_groundwork/src/_arch/aarch64/cpu/boot.rs
@@ -74,10 +74,10 @@ unsafe fn prepare_el2_to_el1_transition(phys_boot_core_stack_end_exclusive_addr:
 /// # Safety
 ///
 /// - Exception return from EL2 must must continue execution in EL1 with `kernel_init()`.
-#[no_mangle]
-pub unsafe extern "C" fn _start_rust(phys_boot_core_stack_end_exclusive_addr: u64) -> ! {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn _start_rust(phys_boot_core_stack_end_exclusive_addr: u64) -> ! { unsafe {
     prepare_el2_to_el1_transition(phys_boot_core_stack_end_exclusive_addr);
 
     // Use `eret` to "return" to EL1. This results in execution of kernel_init() in EL1.
     asm::eret()
-}
+}}
