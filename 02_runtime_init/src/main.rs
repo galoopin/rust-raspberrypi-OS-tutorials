1f02f79a1b242cc3b128b18a0e5ab8a3c237f117
Galoopin <aurelien.cibert@gmail.com>
2025-08-18 12:20:58 +0200

    remove obsolete nightly features gate
    `asm_const`, `const_option`, `is_sorted`, `nonzero_min_max`, `panic_info_message` and `unchecked_path` features have been stabilized

--- 02_runtime_init/src/main.rs
+++ 02_runtime_init/src/main.rs
@@ -106,7 +106,6 @@
 //!     - It is implemented in `src/_arch/__arch_name__/cpu/boot.s`.
 //! 2. Once finished with architectural setup, the arch code calls `kernel_init()`.
 
-#![feature(asm_const)]
 #![no_main]
 #![no_std]
 
